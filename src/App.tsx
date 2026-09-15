a050162c9f0ad46048558674bef903c5fe62e5ec
shadownet21 <birkhofxy@gmail.com>
2026-09-15 11:32:42 -0400

    Correction de composant HabitList de App en ajoutant la prop deleteHabit rendant ainsi la suppression opérationnelle.

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export default function App() {
     <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
       <Header />
       <HabitForm addHabit={addHabit} />
-      <HabitList habits={habits} />
+      <HabitList deleteHabit={deleteHabit} habits={habits} />
     </div>
   );
 }
